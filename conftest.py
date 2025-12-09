4d19d40b68511b5e8cac4fb2bc2877acdb22b480
Wuyo269 <wojcik.dabrowa@gmail.com>
2025-12-09 14:46:51 +0100

    ignore utils/__pyache__

--- conftest.py
+++ conftest.py
@@ -7,7 +7,6 @@ import os
 import sys
 
 
-# Ensure repository root is on sys.path so tests can import local packages
 ROOT = os.path.dirname(__file__)
 if ROOT not in sys.path:
     sys.path.insert(0, ROOT)
